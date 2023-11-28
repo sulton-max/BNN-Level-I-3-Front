4fcd64c400b66999ae64208afef3e356d00326a0
Max <sultonbek.rakhimov@gmail.com>
2023-11-28 21:42:52 +0500

    Finish class N77

---
+++ src/N77/tailwind.config.js
@@ -0,0 +1,18 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        textPrimary: '#222222',
+        textSecondary: '#818181',
+        logoSelected: '#000000',
+        logoSecondary: '#717171',
+        logoPrimary: '#ff385c' 
+      }
+    },
+  },
+  plugins: [],
+}

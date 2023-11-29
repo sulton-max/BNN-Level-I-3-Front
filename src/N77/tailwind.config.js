f97e3e672d24621eea49ebac3f0e49cf101ef5af
Max <sultonbek.rakhimov@gmail.com>
2023-11-29 22:35:21 +0500

    Update class N77

--- src/N77/tailwind.config.js
+++ src/N77/tailwind.config.js
@@ -1,18 +1,25 @@
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        textPrimary: '#222222',
-        textSecondary: '#818181',
-        logoSelected: '#000000',
-        logoSecondary: '#717171',
-        logoPrimary: '#ff385c' 
-      }
+    content: [
+        "./index.html",
+        "./src/**/*.{vue,js,ts,jsx,tsx}",
+    ],
+    theme: {
+        screens: {
+            sm: '545px',
+            md: '960px',
+            lg: '1280px',
+        },
+        extend: {
+            colors: {
+                textPrimary: '#222222',
+                textSecondary: '#818181',
+                logoSelected: '#000000',
+                logoSecondary: '#717171',
+                logoAccent: '#dddddd',
+                logoPrimary: '#ff385c',
+                borderSecondary: '#cccccc',
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
